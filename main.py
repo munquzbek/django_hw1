4531fe374d0f49fcecb512b9c2e6106ded221ca8
munquzbek <munchik31@gmail.com>
2023-11-18 21:59:48 -0500

    fix

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
         query_components = parse_qs(urlparse(self.path).query)
-        with open("index.html", "r") as f:
+        with open("index.html", "r", encoding="utf8") as f:
             page_content = f.read()
         self.send_response(200)  # Отправка кода ответа
         # self.send_header("Content-type", "application/json")  # Отправка типа данных, который будет передаваться
